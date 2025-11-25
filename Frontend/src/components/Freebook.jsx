87d16d61abbb365d1b5024485fc46c6108f38ce2
Binisha Khadka <khadkabinisha48@gmail.com>
2025-11-25 23:48:37 +0545

    Free book add to cart

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,72 @@
+
+import React from 'react'
+import list from '../assets/list.json'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Cards from './Cards';
+
+function Freebook() {
+  const filterData = list.filter(data => data.category === "Free")
+
+   var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+      },
+    ],
+  }
+ 
+  return (
+   <>
+   <div className=' max-w-screen-3xl mx-auto md:px-20 px-4'>
+    <div>
+      <h1 className='font-semibold text-xl pb-2'>Free offered course
+    </h1>
+   <p>Books available for free Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+    Mollitia fugiat in suscipit incidunt sit amet iste
+     consequatur odio cum, eius molestias voluptatum
+      similique minima fuga est enim id facilis </p>
+    </div>
+  
+   <div>
+    <Slider {...settings}>
+       {filterData.map((item)=>(
+        <Cards item={item} key={item.id}/>
+        ))}
+      </Slider>
+   </div>
+    </div>
+   </>
+  )
+}
+
+export default Freebook
+
