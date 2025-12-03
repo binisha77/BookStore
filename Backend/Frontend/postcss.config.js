f8cbd0739d24e198bd26cd173188795c655b9c09
Binisha Khadka <khadkabinisha48@gmail.com>
2025-12-03 21:44:19 +0545

    error check

--- Backend/Frontend/postcss.config.js
+++ Backend/Frontend/postcss.config.js
@@ -1,13 +1,8 @@
-// export default {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
 
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
-  ]
-}
+
