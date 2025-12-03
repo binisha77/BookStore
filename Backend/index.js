9add3cca15dc3b3a3fa74935bd11c80e6d631b20
Binisha Khadka <khadkabinisha48@gmail.com>
2025-12-03 20:06:31 +0545

    backend added

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import userRoute from "./route/user.route.js";
 
 import bookRoute from "./route/book.route.js";
+import { fileURLToPath } from "url";
+import path from "path";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -21,6 +23,7 @@ try {
   mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    autoIndex: true 
   });
   console.log("Connected to mongoDB");
 } catch (error) {
@@ -29,6 +32,16 @@ try {
  //define routes here
  app.use("/book",bookRoute)
   app.use("/user",userRoute)
+
+//  code for deployment
+ if(process.env.NODE_ENV ==="production"){
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,"../Frontend/dist","index.html"))
+  })
+ }
 
 
 app.listen(PORT, () => {
